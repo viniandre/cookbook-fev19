71479733d56f144fd3ba8a3c6e13097e9a23b1bd
Felipe Terron <felipe_terron@yahoo.com.br>
2019-02-18 21:50:59 -0300

    Cookbook dia 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'recipes#index'
-  resources :recipes
+  root to: 'recipes#index' 
+  resources :recipes , only: [:new, :edit, :update, :create, :show, :index]
+
 end
